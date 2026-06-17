7eea4c4432ed082cf39e29e27924bb5344e9bd7e
Irina <weedfox053@gmail.com>
2026-06-17 20:34:33 +0300

    React People Table

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {  Navigate, Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { HomePage } from './pages/HomePage';
 import { PeoplePage } from './pages/PeoplePage';
 import { NotFoundPage } from './pages/NotFoundPage';
